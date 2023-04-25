b3e6cb4f321649561c485c4fd135b00768a2e4de
gj3017 <gj3017@163.com>
2023-04-25 21:44:24 +0800

    20230425状态管理

---
+++ lib/providers/products.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://img.zcool.cn/community/011ec25aaa99a1a80120be1479f136.jpg@1280w_1l_2o_100sh.jpg',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://cbu01.alicdn.com/img/ibank/2018/031/433/8807334130_401031504.jpg',
+    ),
+  ];
+
+  List<Product> get items {
+    return _items;
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void addProduct() {
+    // _items.add(value);
+    notifyListeners();
+  }
+}
